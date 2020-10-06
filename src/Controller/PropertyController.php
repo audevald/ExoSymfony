d70fd76a3aee7e082d0bd3b8d31b07a5e4110172
audevald <audeval.denis@gmail.com>
2020-10-06 12:53:09 +0200

    Feat: Contact (incomplet)

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,16 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use App\Entity\Property;
+use App\Form\ContactType;
 use App\Entity\PropertySearch;
-use App\Repository\PropertyRepository;
 use App\Form\PropertySearchType;
+use App\Repository\PropertyRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Notification\ContactNotification;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Knp\Component\Pager\PaginatorInterface;
-use Symfony\Component\HttpFoundation\Request;
 
 
 class PropertyController extends AbstractController
@@ -49,7 +52,7 @@ class PropertyController extends AbstractController
      * @Route("/biens/{slug}-{id}", name="property.show", requirements={"slug": "[a-z0-9\-]*"})
      * @return Response
      */
-    public function show(Property $property, string $slug): Response
+    public function show(Property $property, string $slug, Request $request, ContactNotification $notification): Response
     {
 
         if ($property->getSlug() !== $slug) {
@@ -58,9 +61,27 @@ class PropertyController extends AbstractController
                 'slug' => $property->getSlug()
             ], 301);
         }
+
+        $contact = new Contact();
+        $contact->setProperty($property);
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $notification->notify($contact);
+            $this->addFlash('success', 'Votre email a bien été envoyé.');
+            /*
+            return $this->redirectToRoute('property.show', [
+                'id' => $property->getId(),
+                'slug' => $property->getSlug()
+            ]);
+            */
+        }
+
         return $this->render('property/show.html.twig', [
             'property' => $property,
-            'current_menu' => 'properties'
+            'current_menu' => 'properties',
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Notification/ContactNotification.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Notification;
+
+use Twig\Environment;
+use App\Entity\Contact;
+use Symfony\Component\Mailer\MailerInterface;
+
+class ContactNotification {
+
+    /**
+     * @var MailerInterface
+     */
+    private $mailer;
+
+    /**
+     * @var Environment
+     */
+    private $renderer;
+
+    public function __construct(MailerInterface $mailer, Environment $renderer)
+    {
+        $this->mailer = $mailer;
+        $this->renderer = $renderer;
+    }
+
+    public function notify(Contact $contact)
+    {
+        $message = (new MailerInterface('Agence : ' . $contact->getProperty()->getTitle()))
+            ->setForm('noreply@agence.fr')
+            ->setTo('contact@agence.fr')
+            ->setReplyTo($contact->getEmail())
+            ->setBody($this->renderer->render('emails/contact.html.twig', [
+                'contact' => $contact
+            ]), 'text/html');
+        $this->mailer->send($message);
+    }
+}
